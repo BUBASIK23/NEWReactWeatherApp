8806187049036271b32e312620d0b7172790aa39
BUBASIK23 <106700188+BUBASIK23@users.noreply.github.com>
2022-12-04 15:03:14 +0100

    api banned. committing for deployment and trying

--- weather-app/src/CurrentDate.js
+++ weather-app/src/CurrentDate.js
@@ -15,12 +15,12 @@ export default function CurrentDate (props) {
         "January", "February", "March", "April", "May", "June", "July", "August",
          "September", "October", "November", "December"
       ]
-  let day = days[props.today.getDay()];
- let hour =props.today.getHours();
-    if (hour<10){hour=`0${hour}`};
-   let min=props.today.getMinutes();
-   if (min<10) {min=`0${min}`};
-  let month=months[props.today.getMonth()];
- return <p>{day} {hour}:{min} {month}</p>
- //return <p>working</p>
+  //let day = days[props.today.getDay()];
+ //let hour =props.today.getHours();
+    //if (hour<10){hour=`0${hour}`};
+   //let min=props.today.getMinutes();
+   //if (min<10) {min=`0${min}`};
+  //let month=months[props.today.getMonth()];
+ //return <p>{day} {hour}:{min} {month}</p>
+ return <p>working</p>
 }
