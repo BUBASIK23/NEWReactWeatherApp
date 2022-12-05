7c4956ee442b2585972413241b52be05195c43a3
BUBASIK23 <106700188+BUBASIK23@users.noreply.github.com>
2022-12-05 21:43:01 +0100

    Update CurrentDate.js

--- weather-app/src/CurrentDate.js
+++ weather-app/src/CurrentDate.js
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function CurrentDate (props) {
     console.log(props.currentDate)
-    /*let days = ["Sunday","Monday","Tuesday","Wednesday", "Thursday","Friday","Saturday"];
+    let days = ["Sunday","Monday","Tuesday","Wednesday", "Thursday","Friday","Saturday"];
       let months=["January", "February", "March", "April", "May", "June", "July", "August",
        "September", "October", "November", "December" ]
+
 
   let day = days[props.currentDate.getDay()];
  let hour =props.currentDate.getHours();
@@ -12,7 +13,8 @@ export default function CurrentDate (props) {
    let min=props.currentDate.getMinutes();
    if (min<10) {min=`0${min}`};
   let month=months[props.currentDate.getMonth()];
- return <p>{day} {hour}:{min} {month}  </p>*/
+ return <p>{day} {hour}:{min} {month}  </p>
+// let day = props.currentDate.getDay();
  //return <p>{day}  </p>
- return <p>working</p>
+ //return <p>working</p>
 }
